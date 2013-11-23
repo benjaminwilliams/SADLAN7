0a2bc041645421f7d850d49175528115f6fac80b
Ben Williams <benj@minwilliams.net>
2013-11-23 21:41:57 +1100

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function(){
+	$(window).bind('scroll',function(e){
+    	//parallaxScroll();
+    });      	                   
+}); 
+
+function parallaxScroll(){
+	//var scrolled = $(window).scrollTop();
+	//$('#section-a').css('top',(0-(scrolled*1.2))+'px');
+
+	//$('#section-c').css('top',(0-(scrolled*0.7))+'px');
+}
